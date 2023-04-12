31fa8d2ba90f1d854a857a6c930cf03b05e51847
Samuel Purriños <samuel.purrinos@outlook.com>
2023-04-12 16:52:55 +0200

    Implement circuit-breaker pattern

--- src/main/java/com/uichesoh/user/controllers/UserController.java
+++ src/main/java/com/uichesoh/user/controllers/UserController.java
@@ -2,13 +2,15 @@ package com.uichesoh.user.controllers;
 
 import com.uichesoh.user.entities.User;
 import com.uichesoh.user.service.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
@@ -27,7 +29,19 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
+    @CircuitBreaker(name ="ratingHotelBreaker",fallbackMethod = "ratingHotelFallBack")
     public ResponseEntity<User> getUserById(@PathVariable String id) {
         return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    public ResponseEntity<User> ratingHotelFallBack(String userId, Exception exception){
+    log.info("Inactive service",exception.getMessage());
+    User user = User.builder()
+            .email("root@gmail.com")
+            .name("root")
+            .info("This user is created when a service ius down")
+            .id("1234")
+        .build();
+    return ResponseEntity.ok(user);
     }
 }
--- src/main/java/com/uichesoh/user/entities/User.java
+++ src/main/java/com/uichesoh/user/entities/User.java
@@ -1,10 +1,7 @@
 package com.uichesoh.user.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +10,7 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "users")
 public class User {
